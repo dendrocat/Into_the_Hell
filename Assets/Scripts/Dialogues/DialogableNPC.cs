c030160ef67fc74d537e3b61963618b854f17da7
dmitryh2004 <dhohlinov@mail.ru>
2025-05-10 21:49:37 +0300

    fixed bug with dialogable npc

--- Assets/Scripts/Dialogues/DialogableNPC.cs
+++ Assets/Scripts/Dialogues/DialogableNPC.cs
@@ -12,7 +12,11 @@ public class DialogableNPC : MonoBehaviour, IInteractable
         if (GetComponents<DialogableNPC>().Length > 1)
         {
             Debug.LogError("Many DialogableNPC");
+            return;
         }
+        _hint = Instantiate(_interactImage, transform);
+        _hint.transform.position += new Vector3(.5f, 1f, 0);
+        _hint.SetActive(false);
     }
 
     public void Interact()
@@ -29,13 +33,12 @@ public class DialogableNPC : MonoBehaviour, IInteractable
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() == null) return;
-        _hint = Instantiate(_interactImage, transform);
-        _hint.transform.position += new Vector3(.5f, 1f, 0);
+        _hint.SetActive(true);
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() == null) return;
-        Destroy(_hint);
+        _hint.SetActive(false);
     }
 }
