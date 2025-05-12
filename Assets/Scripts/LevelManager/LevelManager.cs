ff8787b892e65120f0a0def7d95af14f8863972d
Чикунов ДС <dan123dan5@mail.ru>
2025-05-12 13:19:27 +0300

    Added Level Room and LevelManager Documentation

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,27 +1,28 @@
 using UnityEngine;
 
-public class LevelManager : AbstractLevelManager
-{
-    Level _level;
+/// <summary> Класс, который отвечает за создание и генерацию уровня </summary>
+public class LevelManager : AbstractLevelManager {
+  /// <summary> Уровень <see cref="Level"> </summary>
+	Level _level;
 
-    protected override void InitLevel()
-    {
-        _level = new(new GameObject("Rooms").transform);
-    }
+  /// <summary> Функция инициализации уровня </summary>
+	protected override void InitLevel() {
+		_level = new(new GameObject("Rooms").transform);
+	}
 
-    public override void Generate()
-    {
-        if (_level.created()) return;
+  /// <summary> Функция вызова генерации уровня </summary>
+	public override void Generate() {
+		if (_level.created()) return;
 
-        Generator.New()
-                .SetUpRooms(_levelStorage.GetRoomContainer(_location), isLastLevel)
-                .Halls(_levelStorage.GetHalls())
-                .GenerateRooms(15, _level)
-                .ActivateLevel(_level,
-                    _levelStorage.GetTilesContainer(_location),
-                    _levelStorage.GetTrapContainer(_location)
-                );
+		Generator.New()
+				.SetUpRooms(_levelStorage.GetRoomContainer(_location), isLastLevel)
+				.Halls(_levelStorage.GetHalls())
+				.GenerateRooms(15, _level)
+				.ActivateLevel(_level,
+					_levelStorage.GetTilesContainer(_location),
+					_levelStorage.GetTrapContainer(_location)
+				);
 
-        UIManager.Instance.GenerateMap(_level);
-    }
+		UIManager.Instance.GenerateMap(_level);
+	}
 }
